6a7543d410a7f85c6d8491bed201b3220d9c763c
magfirahsalam <tivannisalam11@gmail.com>
2023-09-08 20:24:18 +0700

    Fixed categories

---
+++ Include/scripts/groovy/stepdefinition/Categories.groovy
@@ -0,0 +1,89 @@
+package stepdefinition
+
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+
+
+public class Categories {
+	
+	@Given("user open apps")
+	public void user_open_apps() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User open apps'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	@When("user input available product name")
+	public void user_input_available_product_name() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User input available product name'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	@Then("user can find the available product name")
+	public void user_can_find_the_available_prodcut_name() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User can find the available product'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	
+	
+	@Given("user already open application")
+	public void uuser_already_open_application() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User open apps'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	@When("user input unavailable product name")
+	public void user_input_unavailable_product_name() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User input unavailable product name'), [('unavailableproduct') : 'kacang goreng pahit'],
+	FailureHandling.STOP_ON_FAILURE)
+	}
+	@Then("user cannot find the unavailable product")
+	public void user_cannot_find_the_unavailable_product() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User cannot find the unavailable product'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	
+	
+	
+	@Given("user open app")
+	public void user_open_app() {
+	WebUI.callTestCase(findTestCase('Categories/Step Definition/User open apps'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	@When("user does not input product name")
+	public void user_does_not_input_product_name() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User not input product name'), [('availableproduct') : 'sepatu kaca'],
+	FailureHandling.STOP_ON_FAILURE)
+	}
+	@Then("user cannot find blank product")
+	public void user_cannot_find_blank_product() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User cannot find blank product name'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	
+	
+	@Given("user open  application seconhand")
+	public void user_open_application_seconhand() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User open apps'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	@When("user  input prodcut name with double space")
+	public void user_input_prodcut_name_with_double_space() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User input product only with double space'), [('space') : '  '],
+	FailureHandling.STOP_ON_FAILURE)
+	}
+	@Then("user can find the random product contain double space")
+	public void user_can_find_the_random_product_contain_double_space() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User can find random product contain space only'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	
+	
+	
+	
+	@Given("user already open apps")
+	public void user_already_open_apps() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User open apps'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	@When("user select category")
+	public void user_select_category() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User select category'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	@Then("user can find the product by the category")
+	public void user_can_find_the_product_by_the_category() {
+		WebUI.callTestCase(findTestCase('Categories/Step Definition/User can find the product by category'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+}
